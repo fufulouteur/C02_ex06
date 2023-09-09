db822e6da4323936526a51ce918d6b136f10e9a4
fufulouteur <manzodqb@icloud.com>
2023-09-09 10:23:24 +0000

    fini

---
+++ ft_str_is_printable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_str_is_printable(char *str) {
+  if (str == NULL)
+    return 0;
+
+  if (*str == '\0')
+    return 1;
+
+  while (*str != '\0') {
+    if (*str < ' ' || *str > '~')
+      return 0;
+    str++;
+  }
+  return 1;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int ft_str_is_printable(char *str);
+
+int main() {
+  printf("%i\n", ft_str_is_printable("coucou"));
+  return 0;
+}
