dfcd79118967905e44a6723272b0ca73ade109fe
fufulouteur <manzodqb@icloud.com>
2023-09-09 10:41:33 +0000

    fini

--- ft_str_is_printable.c
+++ ft_str_is_printable.c
@@ -8,6 +8,8 @@ int ft_str_is_printable(char *str) {
     return 1;
 
   while (*str != '\0') {
+    if (*str == '\n')
+      continue;
     if (*str < ' ' || *str > '~')
       return 0;
     str++;
--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int ft_str_is_printable(char *str);
 
 int main() {
-  printf("%i\n", ft_str_is_printable("coucou"));
+  printf("%i\n", ft_str_is_printable("coucou\0\n"));
   return 0;
 }
